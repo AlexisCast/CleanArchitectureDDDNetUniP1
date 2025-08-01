746b8a1aee35bad1f87173705b5527b00896b3f6
Alexis Castellanos <castellanos.alexis@hotmail.com>
2025-08-01 01:45:47 -0600

    feat 2: Domain Layer - Create Entity in Domain

---
+++ src/CleanArchitecture/CleanArchitecture.Domain/Vehicles/Vehicle.cs
@@ -0,0 +1,19 @@
+namespace CleanArchitecture.Domain.Vehicles;
+
+public sealed class Vehicle
+{
+    public Guid Id { get; private set; }
+    public string? Model { get; private set; }
+    public string? Vin { get; private set; }
+    public string? Street { get; private set; }
+    public string? Department { get; private set; }
+    public string? Province { get; private set; }
+    public string? City { get; private set; }
+    public string? Country { get; private set; }
+    public string Price { get; private set; }
+    public string? CurrencyType { get; private set; }
+    public decimal Mantainance { get; private set; }
+    public string? MaintenanceCurrencyType { get; private set; }
+    public DateTime? LastRentalDate { get; private set; }
+    public List<Accessory> Accessories { get; private set; } = new();
+}
